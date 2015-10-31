b2487bb10915f695ae5a716a019d2d0166b1c3ca
John Mac <johngrantuk@googlemail.com>
2015-10-31 14:17:58 +0000

    Starting step 4.

---
+++ client/scripts/controllers/chat.controller.ng.js
@@ -0,0 +1,16 @@
+angular
+  .module('Fitsapp')
+  .controller('ChatsCtrl', ChatsCtrl);
+
+function ChatsCtrl ($scope) {
+
+  $scope.chats = $scope.$meteorCollection(Chats, false);
+
+  $scope.remove = remove;
+
+  ////////////
+
+  function remove (chat) {
+    $scope.chats.remove(chat);
+  }
+}
